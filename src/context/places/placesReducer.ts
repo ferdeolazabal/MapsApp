87abe856dce768d6a2cd3e5368d5c4bfc04bf025
ferdeolazabal <ferdeolazabal@gmail.com>
2022-05-13 00:16:34 -0300

    initial state initialized

--- src/context/places/placesReducer.ts
+++ src/context/places/placesReducer.ts
@@ -1,4 +1,21 @@
+import { PlacesState } from './PlacesProvider';
 
-export const placesReducer = () => {
+type PlacesAction = {
+    type: 'setUserLocation', payload: [ number, number ];
+}
+
+
+export const placesReducer = ( state: PlacesState, { type, payload }: PlacesAction ): PlacesState => {
+
+    switch (type) {
+        case 'setUserLocation':
+            return {
+                ...state,
+                isLoading: false,
+                userLocation: payload
+            }
+        default:
+            return state;
+    }
 
 }
