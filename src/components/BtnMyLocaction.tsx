9805a44d506046b6ed7d15fcb34ef9e5aa7e4902
ferdeolazabal <ferdeolazabal@gmail.com>
2023-07-11 19:38:39 -0300

    Delete legend text

--- src/components/BtnMyLocaction.tsx
+++ src/components/BtnMyLocaction.tsx
@@ -2,34 +2,31 @@ import { useContext } from "react";
 import { MapContext, PlacesContext } from "../context";
 
 export const BtnMyLocaction = () => {
+  const { map, isMapReady } = useContext(MapContext);
+  const { userLocation } = useContext(PlacesContext);
 
-    const { map, isMapReady } = useContext( MapContext );
-    const { userLocation } = useContext( PlacesContext );
+  const onClick = () => {
+    if (!isMapReady || !userLocation) return;
 
-    const onClick = () => {
+    map?.flyTo({
+      zoom: 14,
+      center: userLocation,
+    });
+  };
 
-        if( !isMapReady || !userLocation ) return;
-
-        map?.flyTo({
-            zoom: 14,
-            center: userLocation
-        });
-
-    }
-
-    return (
-        <button 
-            style={{
-                cursor: 'pointer',
-                position: 'fixed',
-                right: '20px',
-                top: '20px',
-                zIndex: 999,
-            }}
-            onClick={ onClick }
-            className="btn btn-primary"
-        >
-            <i className="bi bi-record-circle"> Mi Ubicación </i>
-        </button>
-    )
-}
+  return (
+    <button
+      style={{
+        cursor: "pointer",
+        position: "fixed",
+        right: "20px",
+        top: "20px",
+        zIndex: 999,
+      }}
+      onClick={onClick}
+      className="btn btn-primary"
+    >
+      <i className="bi bi-record-circle" />
+    </button>
+  );
+};
